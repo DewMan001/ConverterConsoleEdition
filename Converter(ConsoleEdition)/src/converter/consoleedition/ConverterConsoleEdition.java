9b36a47d04077847b1829c5ae1763dd56a42734d
DewMan001 <deweymanster@gmail.com>
2016-05-28 21:03:52 +0800

    Initial commit
    An endeavour to create a console version of the converter

---
+++ Converter(ConsoleEdition)/src/converter/consoleedition/ConverterConsoleEdition.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter.consoleedition;
+import java.util.Scanner;
+/**
+ *
+ * @author James
+ */
+public class ConverterConsoleEdition
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        //setting up the scanner
+        Scanner readInput = new Scanner(System.in);
+        //setting up the intro in an output string
+        String outputIntro;
+
+        outputIntro = "Hello! Welcome to the converter!" + "\n\n";
+        outputIntro += "What would you like to convert today?" + "\n";
+        outputIntro += "1. Distance" + "\n";
+        outputIntro += "2. Temperature" + "\n";
+
+        System.out.println(outputIntro);
+        
+        int userInput = Integer.parseInt(readInput.next());
+        
+        if (userInput == 1)
+        {
+            Distance conDistance = new Distance();
+            
+            String outputDistanceFrom;
+            outputDistanceFrom = "What would you like to convert from?" + "\n\n";
+        }
+    }
+    
+}
